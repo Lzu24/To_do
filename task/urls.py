8d701e53dd6d5e7e8345c0e787dc58e0e2f4bd21
Lzu24 <l.zub24@gmail.com>
2021-12-13 17:00:41 +0100

    First commit

---
+++ task/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="lst"),
+    path('update_task/<str:pk>/', views.updateTask, name="update.task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
+    
+
+]
